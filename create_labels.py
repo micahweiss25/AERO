399df3d3003228a1a87caf62521c0d1e241ab44a
justin.suess <justin.suess@westpoint.edu>
2022-04-10 15:40:08 -0400

    added changes

--- create_labels.py
+++ create_labels.py
@@ -19,9 +19,11 @@ for filename in os.listdir(directory):
 # ids becomes a list of all the "id" in the json file
 
 import json
+import numpy as np
 j = json.load(open('test.json'))
 jt  = json.load(open('train.json'))
-a = []
-for i in j['annotations']:
-    a.append(i['category_id'])
+a = np.array([])
+for i in jt['annotations']:
+    a = np.append(a,i['category_id'])
+np.savetxt('annotations_test_category_id.npy',a)
 print(a)
